e68ce310c57285d5b577624da4d77e8d4cd3b9ed
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-06 17:33:17 +0200

    DiceRoller

--- 364 Dice Roller/DiceRoller.py
+++ 364 Dice Roller/DiceRoller.py
@@ -1,1 +1,16 @@
-import random
+#https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+from random import randint
+
+input = ['5d12', '6d4', '1d2', '1d8', '3d6', '4d20', '100d100']
+
+for i in input:
+    total = 0
+    rolls = ''
+    d = i.split('d')
+    amount = int(d[0])
+    faces = int(d[1])
+    for a in range(0, amount):
+        rint = randint(1, faces)
+        total += rint
+        rolls += str(rint) + ' '
+    print(f'{total}: {rolls}')
